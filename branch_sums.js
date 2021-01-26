b5aa71bfc27ca4cc7b6525b8e71c9c2af6440f09
Jose Gaston <jmcbgaston@gmail.com>
2021-01-25 16:22:07 -0800

    Supplement branch sum with notes

--- branch_sums.js
+++ branch_sums.js
@@ -1,22 +1,29 @@
 // time o(n)
-// space o(log n)
+// space o(n)
 
 function branchSums(root) {
+    // reframe the problem
     let sums = [];
     calcBranchSums(root, 0, sums);
+
+    // return array of sums
     return sums;
 }
 
 function calcBranchSums(node, runningSum, sums) {
+    // if node.value === null, return
     if (!node) return;
 
+    // keep track of the running sum by adding passed in argument as well as the current node's value
     let newRunningSum = runningSum + node.value
 
+    // check to see if the node is a leaf node | if yes then push running sum into sums array
     if (!node.left && !node.right) {
         sums.push(newRunningSum);
 			return;
     }
 
+    // recursive call will handle tracking and itteration till the end
     calcBranchSums(node.left, newRunningSum, sums);
     calcBranchSums(node.right, newRunningSum, sums);
 }
