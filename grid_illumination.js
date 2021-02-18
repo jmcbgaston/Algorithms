81655af88e26f7de59a8953ab82df4af80d18584
Jose Gaston <jmcbgaston@gmail.com>
2021-02-18 15:20:50 -0800

    Add Dropbox Demo Question

---
+++ grid_illumination.js
@@ -0,0 +1,11 @@
+// DropBox HackerRank challenge
+
+function checkIllumination(N, lamps, queries) {
+
+    // N = size NxN of grid
+    // lamps = [[r, c], [r, c]] lamp coordinates
+    // queries = list of interesting cells
+    
+    
+     
+    }
