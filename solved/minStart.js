10d0b48d3ef978b6834390caee718d1e48d2c213
Jose Gaston <jmcbgaston@gmail.com>
2021-03-19 09:17:58 -0700

    Update

---
+++ solved/minStart.js
@@ -0,0 +1,22 @@
+function minStart(arr) {
+    for (let n = 1; n < Infinity; n++) {
+      if (runTest(arr, n)) {
+        return n
+      }
+    }
+  }
+  
+  function runTest(arr, testNum) {
+    let runningSum = testNum
+    let bool = true
+    arr.forEach(num => {
+      runningSum += num
+      if (runningSum < 1) {
+        bool = false
+      }
+    })
+    return bool
+  }
+  
+  let arr = [-4, 3, 2, 1]
+  console.log(minStart(arr))
